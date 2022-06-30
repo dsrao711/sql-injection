c87f15143448e02ea78a942e4bcd74d4e3752358
dsrao711 <dsrao0712@gmail.com>
2022-07-01 02:21:07 +0530

    fix : SQL injection issue fixed

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const mysqlConnection = require('./connection');
 
+// Get people by id API 
 app.get('/getPeople', (req, res) => {
-    const query = `SELECT * FROM persons `;
-    mysqlConnection.query(query, (err, rows, fields) => {
+    const id = req.query.id
+        // const query = `SELECT * FROM persons WHERE PersonId = ${id}`;
+    const query = `SELECT * FROM persons WHERE PersonId = ?`;
+    mysqlConnection.query(query, [id], (err, rows, fields) => {
         if (!err) {
             res.status(200)
                 .send(rows)
@@ -14,9 +17,28 @@ app.get('/getPeople', (req, res) => {
                 .send(fields)
         }
     })
-
 })
 
+// get people by age == 29 and input city
+app.get("/getByCityAndAge", (req, res) => {
+    const city = req.query.city
+        // const query = `SELECT * FROM persons WHERE city = ${city} AND Age = 29`;
+    const query = `SELECT * FROM persons WHERE city = ? AND Age = 29`;
+    mysqlConnection.query(query, [city], (err, rows, fields) => {
+        if (!err) {
+            res.status(200)
+                .send(rows)
+        } else {
+            console.log(err)
+            res.status(400)
+                .json({ message: "Please send ip in correct format" })
+        }
+    })
+})
+
+
+
+// 
 app.listen(3000, () => {
     console.log("Server is running at port 3000..")
 })
