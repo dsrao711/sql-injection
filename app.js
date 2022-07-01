9164d7583a1982dae995e47047b0b6afa49272ba
dsrao711 <dsrao0712@gmail.com>
2022-07-02 02:02:56 +0530

    done

--- app.js
+++ app.js
@@ -24,21 +24,25 @@ app.get("/getByCityAndAge", (req, res) => {
     const city = req.query.city
         // const query = `SELECT * FROM persons WHERE city = ${city} AND Age = 29`;
     const query = `SELECT * FROM persons WHERE city = ? AND Age = 29`;
-    mysqlConnection.query(query, [city], (err, rows, fields) => {
-        if (!err) {
-            res.status(200)
-                .send(rows)
-        } else {
-            console.log(err)
-            res.status(400)
-                .json({ message: "Please send ip in correct format" })
-        }
-    })
+    const onlyLettersPattern = /^[A-Za-z]+$/;
+    if (!query.match(onlyLettersPattern)) {
+        return res.status(400)
+            .json({ err: "No special characters and no numbers, please!" })
+    } else {
+        mysqlConnection.query(query, [city], (err, rows, fields) => {
+            if (!err) {
+                res.status(200)
+                    .send(rows)
+            } else {
+                console.log(err)
+                res.status(400)
+                    .json({ message: "Please send ip in correct format" })
+            }
+        })
+    }
+
 })
 
-
-
-// 
 app.listen(3000, () => {
     console.log("Server is running at port 3000..")
 })
